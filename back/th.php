224faff2bc34f510631c50d4018dd503688433c2
DuskEVE <x987000@gmail.com>
2024-01-31 11:38:01 +0800

    finish basic layout of product manage page

---
+++ back/th.php
@@ -0,0 +1,54 @@
+
+<h2 class="ct">商品分類</h2>
+<div class="ct">
+    新增大分類
+    <input type="text">
+    <button>新增</button>
+</div>
+<div class="ct">
+    新增中分類
+    <input type="text">
+    <button>新增</button>
+</div>
+
+<table class="all">
+    <tr class="tt">
+        <td></td>
+        <td class="ct">
+
+        </td>
+    </tr>
+    <tr class="pp ct">
+        <td></td>
+        <td>
+
+        </td>
+    </tr>
+</table>
+
+<h2 class="ct">商品管理</h2>
+<div class="ct">
+    <button>新增商品</button>
+</div>
+
+<table class="all">
+    <tr class="tt ct">
+        <td>編號</td>
+        <td>商品名稱</td>
+        <td>庫存量</td>
+        <td>狀態</td>
+        <td>操作</td>
+    </tr>
+    <tr class="pp">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td class="ct">
+            <button>修改</button>
+            <button>刪除</button><br>
+            <button>上架</button>
+            <button>下架</button>
+        </td>
+    </tr>
+</table>
