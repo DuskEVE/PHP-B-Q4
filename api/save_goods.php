d0c4169f1ff0f39c1621f6d5a6483a9bea343fc4
DuskEVE <x987000@gmail.com>
2024-02-19 14:16:58 +0800

    adjust save_goods.php in api

--- api/save_goods.php
+++ api/save_goods.php
@@ -5,8 +5,10 @@ if(isset($_FILES['file']['name'])){
     move_uploaded_file($_FILES['file']['tmp_name'], "../img/{$_FILES['file']['name']}");
     $_POST['img'] = $_FILES['file']['name'];
 }
-$_POST['no'] = rand(100000, 999999);
-$_POST['display'] = 1;
+if(!isset($_POST['id'])){
+    $_POST['no'] = rand(100000, 999999);
+    $_POST['display'] = 1;
+}
 $Goods->update($_POST);
 
 header("location:../admin.php?do=th");
